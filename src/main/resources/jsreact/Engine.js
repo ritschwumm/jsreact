0cd9aeecee4804ad7d5641479c31e77aa0ea9b9a
Herr Ritschwumm <ritschwumm@djini.de>
2015-12-23 16:57:20 +0100

    rework observing

--- src/main/resources/jsreact/Engine.js
+++ src/main/resources/jsreact/Engine.js
@@ -2,44 +2,6 @@ var jsreact	= jsreact || {};
 
 jsreact.Engine	= {
 	//------------------------------------------------------------------------------
-	//## observation
-	
-	/** returns a disconnect function */
-	// (Reactive[T], Handler[T]) -> () -> Boolean
-	observe: function(reactive, handler) {
-		this.observeOnce(reactive, handler);
-		return this.subscribe(
-			this.observeAgain.bind(this, reactive, handler)
-		);
-	},
-	
-	/** call this to immediately observe a signal */
-	observeOnce: function(reactive, handler) {
-		reactive.update();
-		reactive.notify(true, handler);
-	},
-	
-	observeAgain: function(reactive, handler) {
-		reactive.update();
-		reactive.notify(false, handler);
-	},
-	
-	//------------------------------------------------------------------------------
-	//## feedback
-	
-	/** returns a disconnect function */
-	// (Stream[T], Cell[T]) -> () -> Boolean
-	feedCell: function(stream, cell) {
-		return this.observe(stream, cell.set.bind(cell));
-	},
-	
-	/** returns a disconnect function */
-	// (Stream[T], Cell[T]) -> () -> Boolean
-	feedEmitter: function(stream, emitter) {
-		return this.observe(stream, emitter.emit.bind(emitter));
-	},
-	
-	//------------------------------------------------------------------------------
 	//## private
 	
 	subscribers:	[],
@@ -61,11 +23,13 @@ jsreact.Engine	= {
 		this.propagating	= false;
 	},
 	
+	// (() -> Unit) -> (() -> Unit)
 	subscribe: function(subscriber) {
 		this.subscribers.push(subscriber);
 		return this.unsubscribe.bind(this, subscriber);
 	},
 	
+	// (() -> Unit) -> Boolean
 	unsubscribe: function(subscriber) {
 		var index	= this.subscribers.indexOf(subscriber);
 		var found	= index !== -1;
---
+++ src/main/resources/jsreact/Observing.js
@@ -0,0 +1,70 @@
+var jsreact	= jsreact || {};
+
+/** alive unzil the alive signal becomes false */
+// Signal[Boolean] -> Observing
+jsreact.Observing	= function Observing(aliveSignal) {
+	this.aliveSignal	= aliveSignal;
+};
+jsreact.Observing.prototype	= {
+	/** alive while this Observing is alive and another Signal didn't become false */
+	child: function(aliveSignal) {
+		var childAlive	=
+				jsreact.Signals.combine(
+					function(a,b) { return a && b; },
+					this.aliveSignal,
+					aliveSignal
+				);
+		return new jsreact.Observing(childAlive);
+	},
+	
+	// (Reactive[T], Handler[T]) -> Unit
+	observe: function(reactive, handler) {
+		var self	= this;
+		
+		self.aliveSignal.update();
+		if (self.aliveSignal.value) {
+			self.updateAndNotify(reactive, handler, true);
+			var unsubscribe	= jsreact.Engine.subscribe(function() {
+				self.aliveSignal.update();
+				if (self.aliveSignal.value) {
+					self.updateAndNotify(reactive, handler, false);
+				}
+				else {
+					unsubscribe();
+				}
+			});
+		}
+	},
+	
+	// (Reactive[T], Handler[T]) -> Unit
+	initialize: function(reactive, handler) {
+		this.aliveSignal.update();
+		if (this.aliveSignal.value) {
+			this.updateAndNotify(reactive, handler, true);
+		}
+	},
+	
+	// (Stream[T], Cell[T]) -> Unit
+	feedCell: function(stream, cell) {
+		return this.observe(stream, cell.set.bind(cell));
+	},
+	
+	// (Stream[T], Cell[T]) -> Unit
+	feedEmitter: function(stream, emitter) {
+		return this.observe(stream, emitter.emit.bind(emitter));
+	},
+	
+	//------------------------------------------------------------------------------
+	//## private
+	
+	// (Reactive[T], Handler[T]) -> Unit
+	updateAndNotify: function(reactive, handler, first) {
+		reactive.update();
+		reactive.notify(first, handler);
+	}//,
+};
+jsreact.Observing.always	= function() {
+	return new jsreact.Observing(
+		jsreact.Signals.constant(true)	
+	);
+};
